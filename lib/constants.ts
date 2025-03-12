b360df0edee31cbef759dc07aa50924a05562870
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2025-03-12 11:42:37 -0700

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,9 @@
 export const DOODSTREAM_BASE_URL =
     process.env.DOODSTREAM_BASE_URL || "https://doodapi.com";
 export const DOODSTREAM_API_KEY = process.env.DOODSTREAM_API_KEY;
+export const STREAMTAPE_BASE_URL =
+    process.env.STREAMTAPE_BASE_URL || "https://api.streamtape.com";
+export const STREAMTAPE_API_KEY = process.env.STREAMTAPE_API_KEY;
 export const SITENAME = process.env.SITENAME || "DoodWeb";
 export const DEFAULT_PER_PAGE = parseInt(process.env.DEFAULT_PER_PAGE || "40");
 export const DEFAULT_REVALIDATE_INTERVAL = 3600;
