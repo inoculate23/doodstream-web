68d75aa1c5906cd719a295c2f626c77e4f23145f
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2025-03-12 12:52:15 -0700

    Update streamtape.ts

--- lib/streamtape.ts
+++ lib/streamtape.ts
@@ -4,29 +4,37 @@ import {
     STREAMTAPE_API_KEY,
     STREAMTAPE_BASE_URL,
     STREAMTAPE_LOGIN,
+STREAMTAPE_FOLDER,
 } from "./constants";
 
 type STREAMTAPEProps = {
     baseUrl?: string;
+    login?: string;
     key?: string;
+    folder?: string;
 };
 
 class STREAMTAPE {
     baseUrl: string;
     key: string;
+    login: string;
+    folder: string;
+
     upstream: string | undefined;
 
     constructor(
-        { baseUrl, key }: STREAMTAPEProps = {
+        { baseUrl, login, key }: STREAMTAPEProps = {
             baseUrl: undefined,
             login: undefined,
             key: undefined,
+            folder: undefined,
         }
     ) {
         baseUrl = baseUrl || STREAMTAPE_BASE_URL;
         login = login || STREAMTAPE_LOGIN;
         key = key || STREAMTAPE_API_KEY;
-
+        folder = folder || STREAMTAPE_FOLDER;
+        
         if (!baseUrl) throw new Error("STREAMTAPE Base URL not set");
         if (!login) throw new Error("STREAMTAPE Login not set");
         if (!key) throw new Error("STREAMTAPE Key not set");
@@ -34,6 +42,7 @@ class STREAMTAPE {
         this.baseUrl = baseUrl;
         this.login = login;
         this.key = key;
+        this.folder = folder;
     }
 
     serializeQueryParams(params: { [key: string]: string }) {
@@ -58,11 +67,11 @@ class STREAMTAPE {
     async listFiles({
         page = 1,
         per_page = DEFAULT_PER_PAGE,
-        fld_id = "KV5JjG6Ea_4",
+        folder = folder,
     }: {
         page?: number;
         per_page?: number;
-        fld_id?: string;
+        folder?: string;
     }) {
         if (per_page && per_page > 200)
             throw new Error("per_page cannot be greater than 200");
@@ -72,7 +81,7 @@ class STREAMTAPE {
             {
                 page: page.toString(),
                 per_page: per_page.toString(),
-                fld_id: fld_id.toString(),
+                folder: folder.toString(),
             },
             60
         );
@@ -93,18 +102,18 @@ class STREAMTAPE {
         return data;
     }
 
-    async listFolders({ fld_id = "" }: { fld_id?: string }) {
+    async listFolders({ folder = "" }: { folder?: string }) {
         const data = await this.fetch("/folder/list", {
             only_folders: "1",
-            fld_id,
+            folder,
         });
         return data;
     }
 
-    async getFolder({ fld_id }: { fld_id: string }) {
-        const data = await this.listFolders({ fld_id: "" });
+    async getFolder({ folder }: { folder: string }) {
+        const data = await this.listFolders({ folder: "" });
         const folder = data.result.folders.find(
-            (f: any) => f.fld_id === fld_id
+            (f: any) => f.folder === folder
         );
         return {
             ...data,
